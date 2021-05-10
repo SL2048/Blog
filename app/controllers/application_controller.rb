65397ecb9753cf606324d0d4f246875d8e7ba8b8
Eslam <e.ebrahim.1994@gmail.com>
2021-05-10 04:24:22 +0200

    setting up will paginate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+
+  protected
+
+  def pagination_data(objects)
+    {
+      current_page: objects.current_page,
+      next_page: objects.next_page,
+      prev_page: objects.previous_page,
+      total_pages: objects.total_pages,
+      total_count: objects.total_entries
+    }
+  end
+
 end
